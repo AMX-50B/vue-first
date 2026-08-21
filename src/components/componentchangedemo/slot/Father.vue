9a4bedaed04c606f08a8a33e91d1c00638cbb501
LiuY <young.liu@knx.com.cn>
2026-08-21 17:51:29 +0800

    0821

---
+++ src/components/componentchangedemo/slot/Father.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="father">
+        
+    </div>
+   
+</template>
+<script setup lang="ts" name="Father">
+import { ref,reactive } from 'vue';
+import Category from './Category.vue';
+
+</script>
+<style scoped>
+.father{
+    width: 100%;
+    height: 100%;
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+}
+</style>
